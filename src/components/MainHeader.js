a16c29e42bd950ddf52986c4c6111c68186f0dda
RahulSharma <rahulmlc2003@gmail.com>
2023-12-12 00:37:46 +0530

    using navLink it does not load

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -1,11 +1,33 @@
+// const MainHeader = () => {
+//     return(
+//         <ul>
+//             <li>
+//                 <a href="/Product">Product</a>
+//             </li>
+//             <li>
+//                 <a href="/Welcome">Welcome</a>
+//             </li>
+//         </ul>
+//     );
+// }
+
+import { NavLink } from "react-router-dom";
+
+// export default MainHeader;
+
+
+
+
+
+
 const MainHeader = () => {
     return(
         <ul>
             <li>
-                <a href="/Product">Product</a>
+                <NavLink to="/Product">Product</NavLink>
             </li>
             <li>
-                <a href="/Welcome">Welcome</a>
+                <NavLink to="/Welcome">Welcome</NavLink>
             </li>
         </ul>
     );
