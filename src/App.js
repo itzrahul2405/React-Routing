48e7e879fba498cd7e4f2132665c6299b39a2162
RahulSharma <rahulmlc2003@gmail.com>
2023-12-12 00:34:33 +0530

    using anchor tag it does load for a while when switch pages

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import './App.css';
-import React from 'react'
-import { Route } from 'react-router-dom';
-import Welcome from './components/Welcome';
-import Product from './components/Products';
+import "./App.css";
+import React from "react";
+import { Route } from "react-router-dom";
+import Welcome from "./components/Pages/Welcome";
+import Product from "./components/Pages/Products";
+import MainHeader from "./components/MainHeader";
 
 function App() {
   return (
     <div className="App">
-      <Route path='/Welcome'>
-        <Welcome />
-      </Route>
-      <Route path='/Product'>
-        <Product />
-      </Route>
+      <MainHeader />
+      <main>
+        <Route path="/Welcome">
+          <Welcome />
+        </Route>
+        <Route path="/Product">
+          <Product />
+        </Route>
+      </main>
     </div>
   );
 }
---
+++ src/components/MainHeader.js
@@ -0,0 +1,14 @@
+const MainHeader = () => {
+    return(
+        <ul>
+            <li>
+                <a href="/Product">Product</a>
+            </li>
+            <li>
+                <a href="/Welcome">Welcome</a>
+            </li>
+        </ul>
+    );
+}
+
+export default MainHeader;
