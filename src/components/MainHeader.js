f526627b40f0387c2c80fa086d759e68abc8d13a
RahulSharma <rahulmlc2003@gmail.com>
2023-12-12 01:01:33 +0530

    navLink

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -18,53 +18,61 @@
 
 
 
-import { Link } from "react-router-dom";
-
-const MainHeader = () => {
-    return(
-        <ul>
-            <li>
-                <Link to="/Product">Product</Link>
-            </li>
-            <li>
-                <Link to="/Welcome">Welcome</Link>
-            </li>
-        </ul>
-    );
-}
-
-export default MainHeader;
 
 
 
 
 
 
+// Link is a basic navigation component, while NavLink extends Link with additional features for styling and handling the "active" state, making it suitable for navigation menus with dynamic styles.
 
-
-
-
-
-
-
-// import { NavLink } from "react-router-dom";
-// import classes from './MainHeader.module.css'
+// import { Link } from "react-router-dom";
 
 // const MainHeader = () => {
-//   return (
-//     <header className={classes.header}>
-//       <nav>
+//     return(
 //         <ul>
-//           <li>
-//             <NavLink to="/Product">Product</NavLink>
-//           </li>
-//           <li>
-//             <NavLink to="/Welcome">Welcome</NavLink>
-//           </li>
+//             <li>
+//                 <Link to="/Product">Product</Link>
+//             </li>
+//             <li>
+//                 <Link to="/Welcome">Welcome</Link>
+//             </li>
 //         </ul>
-//       </nav>
-//     </header>
-//   );
-// };
+//     );
+// }
 
 // export default MainHeader;
+
+
+
+
+
+
+
+
+
+
+
+
+
+import { NavLink } from "react-router-dom";
+import classes from './MainHeader.module.css'
+
+const MainHeader = () => {
+  return (
+    <header className={classes.header}>
+      <nav>
+        <ul>
+          <li>
+            <NavLink activeClassName={classes.active} to="/Product">Product</NavLink>
+          </li>
+          <li>
+            <NavLink activeClassName={classes.active} to="/Welcome">Welcome</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainHeader;
